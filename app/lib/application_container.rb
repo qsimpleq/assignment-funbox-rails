dcca2b633c7710ac034aa82d5a04ee34eec8fe22
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-10-18 14:24:29 +0500

    Set :ru locale

--- app/lib/application_container.rb
+++ app/lib/application_container.rb
@@ -6,4 +6,6 @@ class ApplicationContainer
   register :redis do
     @register_redis ||= ConnectionPool::Wrapper.new { Redis.new(url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379')) }
   end
+
+  # register :
 end
